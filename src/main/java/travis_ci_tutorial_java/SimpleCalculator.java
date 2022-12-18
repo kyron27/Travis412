4b41ba7793e66e2b4cd2a7e6ba4ad64c68582bae
kyron27 <kabrah3@students.towson.edu>
2022-12-17 22:09:08 -0500

    Added Subtract

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -1,7 +1,12 @@
 package travis_ci_tutorial_java;
 
 public class SimpleCalculator {
+	
 	public int add(int a, int b) {
 		return a + b;
+	}
+	
+	public int subtract(int a, int b) {
+		return a - b;
 	}
 }
